0f19baf49fd49cdc4dce4d1e694fb8029dbbcbbf
user <user@DESKTOP-FTTFP6B>
2024-02-29 12:51:00 +0300

    patched

--- Redi.DataAccess/Migrations/20240229093910_Fixed.Designer.cs
+++ Redi.DataAccess/Migrations/20240229093910_Fixed.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Redi.DataAccess.Data;
 
@@ -11,9 +12,10 @@ using Redi.DataAccess.Data;
 namespace Redi.DataAccess.Migrations
 {
     [DbContext(typeof(RediDbContext))]
-    partial class RediDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240229093910_Fixed")]
+    partial class Fixed
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Redi.DataAccess/Migrations/20240229093910_Fixed.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Redi.DataAccess.Migrations
+{
+    public partial class Fixed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<float>(
+                name: "PackageWeight",
+                table: "Deliveries",
+                type: "real",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PackageWeight",
+                table: "Deliveries",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(float),
+                oldType: "real");
+        }
+    }
+}
